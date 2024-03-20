02c3bc4e6ae9f8fda5aa67712dd9028536b4f20b
MOHD JAVED KHAN <118979505+javed8299@users.noreply.github.com>
2024-03-20 18:00:18 +0530

    updated

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,9 +1,18 @@
 import Button from '../button/button.component';
 import './cart-dropdown.style.scss';
+import { CartContext } from "../../context/cart.context";
+import { useContext } from "react";
+import CartItem from '../cart-item/cart-item.component';
+
 const CartDropdown = () => {
+
+    const { cartItem } = useContext(CartContext)
+
     return (
         <div className='cart-dropdown-container'>
-            <div className='cart-items' />
+            <div className='cart-items'>
+                {cartItem.map(item => <CartItem key={item.id} cartItem={item} />)}
+            </div>
             <Button>GO TO CHECKOUT</Button>
         </div>
     )
--- src/context/cart.context.jsx
+++ src/context/cart.context.jsx
@@ -1,14 +1,32 @@
 import { createContext, useState } from 'react';
 
+const addCartItem = (cartItem, productToAdd) => {
+
+    const  existingCartItem = cartItem.find((cartItems) => cartItems.id === productToAdd.id);
+
+    if(existingCartItem) {
+        return cartItem.map((cartItems) => cartItems.id === productToAdd.id ? {...cartItems, quantity: cartItems.quantity + 1} : cartItems)
+    }
+
+    return [...cartItem, {...productToAdd, quantity: 1}];
+}
 export const CartContext = createContext({
     isCartOpen: false,
-    setIsCartOpen: () => {}
+    setIsCartOpen: () => {},
+    cartItem: [],
+    addItemToCart: () => {}
 })
 
 export const CartProvider = ({children}) => {
 
     const [isCartOpen, setIsCartOpen] = useState(false);
-    const value = { isCartOpen, setIsCartOpen}
+    const [cartItem, setCartItem] = useState([]);
+
+    const addItemToCart = (productToAdd) => {
+        setCartItem(addCartItem(cartItem, productToAdd))
+    }
+
+    const value = { isCartOpen, setIsCartOpen, cartItem, addItemToCart}
 
     return (
         <CartContext.Provider value={value}>{children}</CartContext.Provider>
